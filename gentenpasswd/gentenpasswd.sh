91ad633da386dfc199312adc9fb022c672513b9a
Arthur <cuttlefish.3037@gmail.com>
2019-10-16 16:38:12 +0200

    added some comments and removed unnecessary option

--- gentenpasswd/gentenpasswd.sh
+++ gentenpasswd/gentenpasswd.sh
@@ -7,12 +7,13 @@
 # it deletes (d) everything in the complement (c) of the set of
 # alphanumeric characters; this leaves only alphanumeric characters
 
-# fold folds lines to a width of $1 which is the argument to this
+# fold folds lines to a width (w) of $1 which is the argument to this
 # script
 
-# head displays the first 10 lines
+# head displays the first 10 lines. This is already the default
+# behaviour of head. Option -n10 is unnecessary
 
 # use diceware (http://world.std.com/~reinhold/diceware.html) if you
 # want secure and easy to remember passwords
 
-tr -cd [:alnum:] < /dev/urandom | fold -w $1 | head -n10
+tr -cd [:alnum:] < /dev/urandom | fold -w $1 | head
